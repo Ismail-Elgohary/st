9ec1d44f9de9bd29559dd177b689357d98f915d6
Ismail Elgohary <ismailelgohary173@gmail.com>
2024-11-29 09:37:43 +0200

    files

--- config.h
+++ config.h
@@ -96,34 +96,33 @@ unsigned int tabspaces = 4;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
-	"#3b4252",
-	"#bf616a",
-	"#a3be8c",
-	"#ebcb8b",
-	"#81a1c1",
-	"#b48ead",
-	"#88c0d0",
-	"#e5e9f0",
+	"black",
+	"red3",
+	"green3",
+	"yellow3",
+	"blue2",
+	"magenta3",
+	"cyan3",
+	"gray90",
 
 	/* 8 bright colors */
-	"#4c566a",
-	"#bf616a",
-	"#a3be8c",
-	"#ebcb8b",
-	"#81a1c1",
-	"#b48ead",
-	"#8fbcbb",
-	"#eceff4",
+	"gray50",
+	"red",
+	"green",
+	"yellow",
+	"#5c5cff",
+	"magenta",
+	"cyan",
+	"white",
 
 	[255] = 0,
 
 	/* more colors can be added after 255 to use with DefaultXX */
-	"#d8dee9", // cs
-	"#3b4252", // rcs
-	"#d8dee9", // fg
-	"#242933", // bg
-};
-
+	"#cccccc",
+	"#555555",
+	"gray90", /* default foreground colour */
+	"black", /* default background colour */
+}; 
 
 /*
  * Default colors (colorname index)
